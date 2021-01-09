02cc311bba350983c53b58d4f0ce4720d14eb185
irvanalfi <irvanhidayat0623@gmail.com>
2021-01-09 19:15:05 +0700

    push

--- application/controllers/Home_c.php
+++ application/controllers/Home_c.php
@@ -4,6 +4,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Home_C extends CI_Controller
 {
 
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+
     public function index()
     {
         $this->load->view('home');
