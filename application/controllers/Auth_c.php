30026b2766d3cb43b564b02698c2c47aec592a29
irvanalfi <irvanhidayat0623@gmail.com>
2021-01-09 12:10:34 +0700

    start

---
+++ application/controllers/Auth_c.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth_c extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('xxx');
+    }
+}
---
+++ application/views/splash.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap-4.5.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/animate.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/all.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/flaticon.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/themify-icons.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/nice-select.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/slick.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/splash.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="splash">
+        <div class="img"></div>
+    </div>
+    <script src="<?= base_url() ?>assets/js/jquery-1.12.1.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/popper.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/jquery.magnific-popup.js"></script>
+    <script src="<?= base_url() ?>assets/js/swiper.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/masonry.pkgd.js"></script>
+    <script src="<?= base_url() ?>assets/js/owl.carousel.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/jquery.nice-select.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/slick.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/jquery.counterup.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/waypoints.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/contact.js"></script>
+    <script src="<?= base_url() ?>assets/js/jquery.ajaxchimp.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/jquery.form.js"></script>
+    <script src="<?= base_url() ?>assets/js/jquery.validate.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/mail-script.js"></script>
+
+    <script src="<?= base_url() ?>assets/js/custom.js"></script>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-23581568-13');
+    </script>
+    <script defer src="../../../static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"rayId":"60e9f8576b300be4","version":"2020.12.2","si":10}'></script>
+    <?php redirect('home'); ?>
+</body>
+
+</html>
