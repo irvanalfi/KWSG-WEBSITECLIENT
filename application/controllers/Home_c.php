6f256555cf8d27dc4d7a66f5a40e30f3a4a3ac5a
irvanalfi <irvanhidayat0623@gmail.com>
2021-01-12 18:09:46 +0700

    tampilan home, sidebar, sipintar

---
+++ application/controllers/Home_c.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home_c extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+
+    public function index()
+    {
+        $this->load->view('home');
+    }
+}
---
+++ application/controllers/Sipintar_c.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sipintar_c extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+
+    // public function index()
+    // {
+    //     $this->load->view('home');
+    // }
+
+    public function sipintarKupon()
+    {
+        $this->load->view('sipintarKupon');
+    }
+
+    public function sipintarShu()
+    {
+        $this->load->view('sipintarShu');
+    }
+
+    public function listSipintarKupon()
+    {
+        // iki gae detail sipintar kupon
+        $this->load->view('listSipintarKupon');
+    }
+
+    public function listSipintarShu()
+    {
+        // iki gae detail sipintar shu
+        $this->load->view('listSipintarShu');
+    }
+
+    public function detailSipintarKupon()
+    {
+        // iki gae detail ketika salah satu lis sipintar kupon di pencet
+        $this->load->view('detailSipintarKupon');
+    }
+
+    public function detailSipintarShu()
+    {
+        // iki gae detail ketika salah satu lis sipintar shu di pencet
+        $this->load->view('detailSipintarShu');
+    }
+}
