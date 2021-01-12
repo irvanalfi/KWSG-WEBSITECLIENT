f9a086a7ee335870000ba11a0791e67187057f3e
Alifia Zaida Nurmaya <70618498+AlifiaZaidaNurmaya@users.noreply.github.com>
2021-01-12 19:46:00 +0700

    tampilan login

---
+++ application/controllers/Login_c.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login_c extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+
+    public function index()
+    {
+        $this->load->view('login');
+    }
+}
---
+++ application/models/Login_m.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login_m extends CI_Model
+{
+    public function auth()
+    {
+        # code...
+    }
+}
+?>
