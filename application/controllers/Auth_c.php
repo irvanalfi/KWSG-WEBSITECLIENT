5385d48ae51d150739b3919b769366f119ce34c1
Alifia Zaida Nurmaya <70618498+AlifiaZaidaNurmaya@users.noreply.github.com>
2021-01-15 10:55:16 +0700

    tambah backend login (belum selesai)

--- application/controllers/Auth_c.php
+++ application/controllers/Auth_c.php
@@ -7,7 +7,7 @@ class Auth_c extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        //Do your magic here
+        $this->load->model('Auth_m', 'login');
     }
 
     public function login()
@@ -18,5 +18,28 @@ class Auth_c extends CI_Controller
     public function logout()
     {
         // session_destroy();
+    }
+
+    public function auth()
+    {
+        $user_name = $this->input->post('user_name');
+        $pass = $this->input->post('pass');
+
+        $cekLogin = $this->login->auth($user_name, $pass);
+
+        if ($cekLogin) {
+            foreach ($cekLogin as $row) {
+                $this->session->set_userdata('user_name', $row->user_name);
+                $this->session->set_userdata('pass', $row->pass);
+            }
+
+            redirect('home');
+            
+        } else {
+            $this->session->set_flashdata('pesan', 'No Anggota dan PIN Kartu Sipintar salah!!!');
+            
+            redirect('login');
+            
+        }
     }
 }
---
+++ application/models/Auth_m.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth_m extends CI_Model {
+
+    public function auth($user_name, $pass)
+    {
+        $this->db->select('a.user_name, a.nama, a.pass, b.KD_JAB as kd_jab, c.NM_UNIT as nama_unit, b.KD_UNIT as kd_unit_sdm, c.KD_WILAYAH_AKUN as kd_wilayah_akun, c.KD_GROUP_AKUN as kd_group_akun, c.KD_UNIT_AKUN as kd_unit_akun');
+        $this->db->from('m_pass a');
+        $this->db->join('mas_peg b', 'a.user_name=b.NO_PEG');
+        $this->db->join('tab_unit c', 'b.KD_UNIT=c.KD_UNIT');
+        $this->db->where('a.user_name', $id);
+        $query = $this->db->get();
+        if ($query->num_rows() == 0) {
+            return false;
+        } else {
+            return $query->result_array();
+        }
+    }
+}
+?>
